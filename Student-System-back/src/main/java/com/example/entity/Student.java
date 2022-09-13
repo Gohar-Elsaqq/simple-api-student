16caeb69d7c69e9833f2a6a8bd75e76862d7c0f8
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2022-09-13 10:06:55 +0200

    cmmoit

---
+++ Student-System-back/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,18 @@
+package com.example.entity;
+
+import java.util.Date;
+
+import javax.xml.crypto.Data;
+
+public class Student {
+
+	private int id;
+	private String fullName;
+	private String age;
+	private String address;
+	private Data dataCreated;
+	private Data dataUpData;
+	private Gender gender;
+	
+	
+}
